7bfa0949b9cb85615a9878a53f0f9f4857338284
Salim Mayan <mailsalim@gmail.com>
2021-05-08 15:42:09 -0700

    add router to See List of Vendors

---
+++ VendorAndOrderTracker/Controllers/VendorsController.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VendorAndOrderTracker.Models;
+
+namespace VendorAndOrderTracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll(); //calling method in Models
+      return View(allVendors);
+    }
+
+   
+  }
+}
