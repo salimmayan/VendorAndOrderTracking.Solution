8ff01fb943c240ab3c8df4231d19d682e10180ee
Salim Mayan <mailsalim@gmail.com>
2021-05-08 13:39:18 -0700

    add test for setting Description property of Vendor class

--- VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -6,31 +6,47 @@ using VendorAndOrderTracker.Models;
 
 namespace VendorAndOrderTracker.Tests
 {
-  [TestClass]
-  public class VendorTests : IDisposable
-  {
-
-    public void Dispose()
+    [TestClass]
+    public class VendorTests : IDisposable
     {
-      // Vendor.ClearAll();
+
+        public void Dispose()
+        {
+            // Vendor.ClearAll();
+        }
+
+        [TestMethod]
+        public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+        {
+            Vendor newVendor = new Vendor("test Vendor Name", "trial Vendor desciption");
+            Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+        }
+
+        [TestMethod]
+        public void GetDescription_ReturnDescription_Description()
+        {
+            Vendor newVendor = new Vendor("someName", "someDescription");
+            string description = "someDescription";
+            Assert.AreEqual(description, newVendor.Description);
+        }
+
+        [TestMethod]
+        public void SetDescription_SetDescription_String()
+        {
+            //Arrange
+            string name = "Vendor Name";
+            string description = "Vendor Description";
+            Vendor newVendor = new Vendor(name, description);
+
+            //Act
+            string updatedDescription = "Updated Vendor Description";
+            newVendor.Description = updatedDescription;
+            string result = newVendor.Description;
+
+
+            //Assert
+            Assert.AreEqual(updatedDescription, result);
+        }
+
     }
-
-    [TestMethod]
-    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
-    {
-      Vendor newVendor = new Vendor("test Vendor Name", "trial Vendor desciption");
-      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
-    }
-
-    [TestMethod]
-    public void GetDescription_ReturnDescription_Description ()
-    {
-      Vendor newVendor = new Vendor("someName", "someDescription");
-      string description = "someDescription";
-      Assert.AreEqual(description, newVendor.Description);
-    }
-
-  
-
-  }
 }
