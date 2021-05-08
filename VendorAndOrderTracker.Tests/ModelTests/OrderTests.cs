a27c0f243c2664f63f15bb38541cb70a56cf69b2
Salim Mayan <mailsalim@gmail.com>
2021-05-08 14:27:52 -0700

    add test for Order constructor

--- VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -5,14 +5,21 @@ using System;
 
 namespace VendorAndOrderTracker.Tests
 {
-  [TestClass]
-  public class OrderTest : IDisposable
-  {
-
-    public void Dispose()
+    [TestClass]
+    public class OrderTest : IDisposable
     {
-     
-    }
 
-  }
+        public void Dispose()
+        {
+
+        }
+
+        [TestMethod]
+        public void OrderConstructor_CreatesInstanceOfOrder_Order()
+        {
+            Order newOrder = new Order("someDesc", "someTitle", 10);
+            Assert.AreEqual(typeof(Order), newOrder.GetType());
+        }
+
+    }
 }
--- VendorAndOrderTracker/Models/Order.cs
+++ VendorAndOrderTracker/Models/Order.cs
@@ -1,9 +1,27 @@
 using System.Collections.Generic;
+using System;
 
 namespace VendorAndOrderTracker.Models
 {
     public class Order
     {
+        public string Description { get; set; }
+        public string Title { get; set; }
+
+        public int Price { get; set; }
+        public string Date { get; set; }
+        public static List<Order> _instances = new List<Order> { };
+        public int Id { get; }
+
+        public Order(string description, string title, int price)
+        {
+            Description = description;
+            _instances.Add(this);
+            Id = _instances.Count;
+            Title = title;
+            Price = price;
+            Date = DateTime.Now.ToString("M/d/yyyy");
+        }
 
     }
 }
