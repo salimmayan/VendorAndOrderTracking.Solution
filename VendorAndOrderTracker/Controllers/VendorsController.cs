d3ba2425c07233d679e9d3a38e900e3f3ef5df3c
Salim Mayan <mailsalim@gmail.com>
2021-05-08 16:07:12 -0700

    add router to Add a New vendor

--- VendorAndOrderTracker/Controllers/VendorsController.cs
+++ VendorAndOrderTracker/Controllers/VendorsController.cs
@@ -8,13 +8,24 @@ namespace VendorAndOrderTracker.Controllers
   public class VendorsController : Controller
   {
 
-    [HttpGet("/vendors")]
+    [HttpGet("/vendors")] //service this route when "See list of vendors" is clicked
     public ActionResult Index()
     {
       List<Vendor> allVendors = Vendor.GetAll(); //calling method in Models
-      return View(allVendors);
+      return View(allVendors); //will render name and description for each vendor
     }
 
-   
+   [HttpGet("/vendors/new")]  //service this route when "Add a new vendor" URL is clicked in landing page
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")] //form submission is serviced by this route
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index"); //redirect NOT to home but to "/vendors"
+    }
   }
 }
